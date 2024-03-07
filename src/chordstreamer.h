2a3dadcf21777426ae54be1a0f4677a5a7773575
Ethan Wright <me@wrighte.dev>
2024-03-07 15:18:07 -0800

    Missed h file

---
+++ src/chordstreamer.h
@@ -0,0 +1,13 @@
+#ifndef CHORDSTREAMER_H
+#define CHORDSTREAMER_H
+
+#include <vector>
+#include <string>
+
+namespace chordstreamer {
+
+    std::vector<std::string> compute(const char* audioFilename);
+
+} // namespace chordstreamer
+
+#endif // CHORDSTREAMER_H
